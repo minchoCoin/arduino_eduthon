a45c5e7cc70a01044d4092d1e0e22b9d902d982a
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2023-07-22 14:25:34 +0900

    Update my_servo.h

--- main/my_servo.h
+++ main/my_servo.h
@@ -6,9 +6,14 @@
 
 class head
 {
-  public :
+  public:
+    head(int x_pin, int y_pin); // x축, y축 핀 번호
     void nod(int count); // 고개를 끄덕이기
     void shake(int count); // 고개를 가로젓기
+
+  private:
+    int _x_pin;
+    int _y_pin;
 }
 
 #endif
