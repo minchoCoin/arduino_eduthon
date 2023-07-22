889dc0be845ccf11f62541705aa7192ea16b0f55
minchoCoin <bigteach0508@pusan.ac.kr>
2023-07-22 15:59:17 +0900

    sounddetection

--- main/soundDetection.ino
+++ main/soundDetection.ino
@@ -1,13 +1,16 @@
-//please README.md before programming!
+#define NOISE_PIN A0
 
-#define noisePin A0
+bool isNoise(){
+  return analogRead(NOISE_PIN)>=40;
+}
+
+
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(NOISE_PIN,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
