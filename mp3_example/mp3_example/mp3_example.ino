c4526cee6f6ebdd4b295ac9da2c679dbb6a33d3e
parkjaehong <117131932+ParkJaeHong2000@users.noreply.github.com>
2023-07-22 14:34:53 +0900

    Update mp3_example.ino

--- mp3_example/mp3_example/mp3_example.ino
+++ mp3_example/mp3_example/mp3_example.ino
@@ -1,19 +1,19 @@
-#include <SoftwareSerial.h>             //Library SoftwareSerial
-#include <DFPlayer_Mini_Mp3.h>  //Library DFPlayer mini MP3
- SoftwareSerial serial(10, 11);            // pin TX, RX
- void setup (){
-  serial.begin (9600);  
-  mp3_set_serial (serial);         
-  delay(5); 
-  mp3_set_volume (20);          
-  delay(1000);
+#include <SoftwareSerial.h> //Library SoftwareSerial
+#include <DFPlayer_Mini_Mp3.h> //Library DFPlayer mini MP3
+SoftwareSerial serial(10, 11); // pin TX, RX
+void setup (){
+ serial.begin (9600);
+ mp3_set_serial (serial);
+ delay(5); 
+ mp3_set_volume (20);
+ delay(1000);
 }
 
-void loop (){  
-  mp3_play(1);                         
-  delay(240000);                    
-  mp3_next();
-  mp3_play(2);                        
-  delay(10000);                       
-  mp3_stop();                         
+void loop (){ 
+ mp3_play(1);
+ delay(240000);
+ mp3_next();
+ mp3_play(2);
+ delay(10000);
+ mp3_stop(); 
 }
