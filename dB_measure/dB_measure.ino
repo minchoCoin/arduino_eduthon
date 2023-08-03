33f2964a352372f860e4613e5027a15d3e0a2b3b
minchoCoin <bigteach0508@pusan.ac.kr>
2023-08-03 13:49:22 +0900

    update soundDetection

--- dB_measure/dB_measure.ino
+++ dB_measure/dB_measure.ino
@@ -1,7 +1,7 @@
 int SoundSensor = A0;     // 사운드 센서 설정
 void setup() {               
   Serial.begin(9600); // 시리얼모니터 사용
-  pinMode(SoundSensor,INPUT)
+  pinMode(SoundSensor,INPUT);
 }
 
 void loop() {     
--- main/soundDetection.ino
+++ main/soundDetection.ino
@@ -5,20 +5,17 @@ int getNoiseStep(){
 
   value = value - 1; //error value
 
-  if(value<=20) return 0;
-  else if(20<value&&value<=25) return 1;
-  else if(value<25&&value<=30) return 2;
-  else if(value<30&&value<=35) return 3;
-  else if(value<35&&value<=40) return 4;
-  else if(value<40&&value<=45) return 5;
-  else if(value<45&&value<=50) return 6;
-  else if(value<50&&value<=55) return 7;
-  else if(value<55&&value<=60) return 8;
-  else if(value<60&&value<=65) return 9;
-  else if(value<65&&value<=70) return 10;
-  else if(value<70&&value<=75) return 11;
-  else if(value<75&&value<=80) return 12;
-  else return 13;
+  if(value<50) return 0;
+  else if(50<=value && value<70) return 1;
+  else if(70<=value && value<90) return 2;
+  else if(90<=value && value<110) return 3;
+  else if(110<=value && value<130) return 4;
+  else if(130<=value && value<150) return 5;
+  else if(150<=value && value<170) return 6;
+  else if(170<=value && value<190) return 7;
+  else return 8;
+  
+ 
 }
 void setup() {
   // put your setup code here, to run once:
